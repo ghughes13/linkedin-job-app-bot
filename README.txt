14805abea6fe5fb14922d540f0febb5f11497522
ghughes139 <battletank130@gmail.com>
2024-11-15 08:30:54 -0600

    enabled anti-anti bot countermeasures

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 This is for educational purposes only. Do not actually use this as it's probably against linkedin's ToS.
+
+ToDo:
+1. Apply for easy Apply jobs
+2. Apply for jobs that take you off site.
--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from time import sleep
 from dotenv import load_dotenv
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 import random
@@ -11,11 +12,33 @@ load_dotenv()
 
 class appBot(): 
   def __init__(self):
+
+    #### Driver Setup & Anti-Anti-Bot Countermeasures. 
     options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled") 
+    options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+    options.add_experimental_option("useAutomationExtension", False) 
+
     options.add_experimental_option("detach", True)
     self.driver = webdriver.Chrome(options=options)
+    self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
 
-  def iteration_loop(self): #can delete all of this after bot is finished. Added to improve iteration speed. 
+    userAgentArray = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+]
+
+    for i in range(len(userAgentArray)):
+        # setting User Agent iteratively as Chrome 108 and 107
+        self.driver.execute_cdp_cmd(
+            "Network.setUserAgentOverride", {"userAgent": userAgentArray[i]}
+        )
+        print(self.driver.execute_script("return navigator.userAgent;"))
+        self.driver.get("https://httpbin.io/headers")
+    
+    self.current_jobs_page = 0
+
+  def iteration_loop(self):
     current_el = ''
 
     while True:
@@ -51,32 +74,26 @@ class appBot():
 
   def run_app(self): 
     self.open_linkedin()
-    self.wait_random_time() #wait for site to load
+    # self.wait_random_time() #wait for site to load
 
-    self.navigate_to_signin()
-    self.wait_random_time() #wait for page load before invoking next function.
+    # self.navigate_to_signin()
+    # self.wait_random_time() #wait for page load before invoking next function.
 
-    self.linkedin_login()
-    self.wait_random_time() #wait for login to process
+    # self.linkedin_login()
+    # self.wait_random_time() #wait for login to process
 
-    self.await_security_check_if_present()
+    # self.await_security_check_if_present()
 
-    self.move_to_jobs()
-    self.wait_random_time()
+    # self.move_to_jobs()
+    # self.wait_random_time()
 
-    self.search_matching_jobs()
+    # self.search_matching_jobs()
 
     self.iteration_loop()
 
-  def invoke_method(self, method_name):
-    print(method_name)
-    # Check if the method exists in the instance's dictionary
-    if hasattr(self, method_name):
-        # Get the method and call it
-        method = getattr(self, method_name)
-        method()  # Invoke the method
-    else:
-        print(f"Method '{method_name}' not found.")
+  #################################
+  ########### UTILITIES ###########
+  #################################
 
   def open_linkedin(self):
     self.driver.get('https://www.linkedin.com/')
@@ -89,24 +106,6 @@ class appBot():
       button_one.click()
     else:
       button_two.click()
-
-  def find_element_from_possible_locations(self, possible_locations, aria_label_value):
-    for xpath in possible_locations:
-        try:
-            element = self.driver.find_element('xpath', xpath)
-            aria_label = element.get_attribute("aria-label")
-
-            if aria_label == aria_label_value:
-              print(f"Found element at: {xpath} with aria-label '{aria_label_value}'")
-              return element  # Return the element as soon as it's found with the correct aria-label
-            else:
-              print(f"Element found at: {xpath}, but aria-label '{aria_label}' does not match '{aria_label_value}'")
-              continue  # Go to the next XPath if aria-label does not match
-        except NoSuchElementException:
-            print(f"Element not found at: {xpath}")
-            continue  # Move to the next XPath if element is not found
-    print(f"{aria_label_value} could not be located with any provided XPath.")
-    return None  # Return None if none of the locations work
 
   def linkedin_login(self):
     potential_username_input_locations = ['/html/body/div[1]/main/div[2]/div[1]/form/div[1]/input', '/html/body/div/main/div[2]/div[1]/form/div[1]/input']
@@ -147,7 +146,7 @@ class appBot():
       sleep(5)
     
   def move_to_jobs(self):
-    jobs_button = self.driver.find_element('xpath', '/html/body/div[6]/header/div/nav/ul/li[3]/a')  
+    jobs_button = self.driver.find_by_xpath('//a[contains(@href,"https://www.linkedin.com/jobs/?")]')
     jobs_button.click()
 
   def search_matching_jobs(self):
@@ -157,12 +156,71 @@ class appBot():
     job_title_input.send_keys('Web Developer')
     job_location_input.send_keys('Dallas, Texas, United States')
     job_location_input.send_keys(Keys.ENTER)
+    self.current_jobs_page = 1
 
   def crawl_job_list(self):
-    /html/body/div[6]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul
+    jobs_container = self.driver.find_element('xpath', '/html/body/div[5]/div[3]/div[4]/div/div/main/div/div[2]/div[1]/div/ul')
+    jobs_list = jobs_container.find_elements(By.CSS_SELECTOR, "> li")
 
+    for job in jobs_list:
+      try:
+        # Find and log the h1 text
+        job_title = job.find_element(By.TAG_NAME, "strong")
+        print(f"Job Title: {job_title.text}")
+        
+        # Find and click the button inside the li
+        job_title_link = job.find_element(By.TAG_NAME, "a")
+        # job_title_link.click()
+        
+        # Wait for content to load
+        time.sleep(2)  # Adjust based on loading time or use WebDriverWait
+        
+        # # Interact with the loaded content
+        # loaded_content = driver.find_element(By.CSS_SELECTOR, "div.loaded-content")  # Example selector
+        # print(f"Loaded Content: {loaded_content.text}")
+
+      except Exception as e:
+          print(f"Error processing li: {e}")
+
+  def next_jobs_page(self):
+    next_page = self.driver.find_elements_by_css_selector(f"[aria-label=Page {self.current_jobs_page + 1}]")
+    next_page.click()
+
+  #################################
+  ########### UTILITIES ###########
+  #################################
+
+  #Used to make bot wait random times to seem less bot-like.
   def wait_random_time(self):
-    sleep(random.randint(4,7))
+    sleep(random.randint(3,7))
+
+  #Can probably rework how we select elements to make this obsolete. See move_to_jobs
+  def find_element_from_possible_locations(self, possible_locations, aria_label_value): 
+    for xpath in possible_locations:
+        try:
+            element = self.driver.find_element('xpath', xpath)
+            aria_label = element.get_attribute("aria-label")
+
+            if aria_label == aria_label_value:
+              print(f"Found element at: {xpath} with aria-label '{aria_label_value}'")
+              return element  # Return the element as soon as it's found with the correct aria-label
+            else:
+              print(f"Element found at: {xpath}, but aria-label '{aria_label}' does not match '{aria_label_value}'")
+              continue  # Go to the next XPath if aria-label does not match
+        except NoSuchElementException:
+            print(f"Element not found at: {xpath}")
+            continue  # Move to the next XPath if element is not found
+    print(f"{aria_label_value} could not be located with any provided XPath.")
+    return None  # Return None if none of the locations work
+
+  #For testing or manually invoking functions while app is running
+  def invoke_method(self, method_name): 
+    print(method_name)
+    if hasattr(self, method_name):
+        method = getattr(self, method_name)
+        method()
+    else:
+        print(f"Method '{method_name}' not found.")
 
 
 bot = appBot()
